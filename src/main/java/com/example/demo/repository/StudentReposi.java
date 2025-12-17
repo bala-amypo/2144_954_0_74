3966b09df870549fc1ee5d4184657988fdcd3a70
bala-amypo <bala@amypo.in>
2025-12-17 06:17:52 +0000

    User commit

--- src/main/java/com/example/demo/repository/StudentReposi.java
+++ src/main/java/com/example/demo/repository/StudentReposi.java
@@ -1,8 +1,8 @@
 package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.demo.entity.Student;
+import com.example.demo.entity.StudentEntity;
 @Repository
-public interface StudentReposi extends JpaRepository<Student,Long>{
-    
+public interface StudentReposi extends JpaRepository<StudentEntity,Long>{
+
 }
--- src/main/java/com/example/demo/services/impl/StudentImpl.java
+++ src/main/java/com/example/demo/services/impl/StudentImpl.java
@@ -1,3 +1,21 @@
 package com.example.demo.services.impl;
 import java.util.*;
-import org.springframework.stereotype.serc
+import org.springframework.stereotype.Service;
+import com.example.demo.entity.StudentEntity;
+@Service
+public class StudentImpl implements StudentServices{
+    private final Map<Long,StudentEntity> store=new HashMap<>();
+    private Long counter=1;
+    @Override
+    public StudentEntity insertStudent(StudentEntity st){
+        st.setId(counter++);
+        store.put(st.getId(),st);
+        return st;
+    }
+    @Override
+    public List<StudentEntity> getAllStudents(){
+        return new ArrayList<>(store.values());
+    }
+    @Override
+    public Optional<StudentEntity>
+}
