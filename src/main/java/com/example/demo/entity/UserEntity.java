eeb64b61bff64dd85176b759db4b780fbc03fc43
bala-amypo <bala@amypo.in>
2025-12-18 08:24:51 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -6,39 +6,10 @@ public class UserEntity {
     private String name;
     private String email;
     private String password;
-    private String role; 
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
+    private String role;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPassword() {
-        return password;
-    }
+    public UserEntity() {}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getRole() {
-        return role;
-    }
-    public void setRole(String role) {
-        this.role = role;
-    }
     public UserEntity(Long id, String name, String email, String password, String role) {
         this.id = id;
         this.name = name;
@@ -46,6 +17,26 @@ public class UserEntity {
         this.password = password;
         this.role = role;
     }
-    public UserEntity() {
+
+    public Long getId() {
+         return id;
+          }
+    public void setId(Long id) { 
+    this.id = id; 
     }
+
+    public String getName() { 
+        return name;
+         }
+    public void setName(String name) {
+         this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 }
